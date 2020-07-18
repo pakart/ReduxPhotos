b21ca7fe99db0a73d10b3310fc9e2d6aed156c8e
pakart <obscurexdlol@gmail.com>
2020-07-18 13:01:01 +0300

    initial commit

---
+++ app/app.jsx
@@ -0,0 +1,10 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const redux = require('redux');
+const { Provider } = require('react-redux');
+
+ReactDOM.render(
+    <div>hello</div>,
+    document.getElementById('container'),
+  );
+  
